cfb4d1d930a75360d9b17e582362338806bfe544
JoseS-Dev <joseasantana05@gmail.com>
2026-09-13 13:53:08 -0400

    feat: :sparkles: Añadir comandos para manejar vacantes favoritas y paginación

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from "telegraf";
 import { handlePagination } from "./handlerPagination.js";
+import { commandFavorites, handleFavorite } from "./handlerFavorite.js";
 import { commandJobs } from "./jobs.js";
 import { commandStart } from "./start.js";
 import { commandHelp } from "./help.js";
@@ -7,7 +8,9 @@ import { commandHelp } from "./help.js";
 export function registerCommands(bot: Telegraf){
     bot.command("jobs", commandJobs);
     bot.command("start", commandStart);
+    bot.command("favorites", commandFavorites);
     bot.command("help", commandHelp);
 
     bot.action(/^page:.+/, handlePagination);
+    bot.action(/^fav:.+/, handleFavorite);
 }
